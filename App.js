e9fb238eea64fd8511580d417acb65ee926721c2
HoKhua22 <khua.ho22@student.passerellesnumeriques.org>
2022-01-07 15:36:48 +0700

    Updating code

--- App.js
+++ App.js
@@ -1,17 +1,45 @@
+import React, { useState, useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import BottomTab from './navigation/BottomTab';
 import Centres from './Screens/Centres';
 import More from './Screens/More';
+import LoginScreen from './Screens/Auth/LoginScreen';
+import Splash from './Screens/Auth/Splash';
+import ForgotPass from './Screens/Auth/ForgotPass';
 
 const Stack = createNativeStackNavigator();
 export default function App() {
+  const [timePassed, setTimePassed] = useState(false);
+  useEffect(() => {
+    setTimeout(() => setTimePassed({ timePassed: true }), 10000);
+  }, []);
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName='BottomTab' screenOptions={{headerShown: false}}>
-        <Stack.Screen component={BottomTab} name='BottomTab' />
-        <Stack.Screen component={Centres} name='Centres' />
-        <Stack.Screen component={More} name='More' />
+      <Stack.Navigator
+      // initialRouteName='BottomTab' screenOptions={{headerShown: false}}
+      >
+        {timePassed ? (
+          <Stack.Screen
+            name="Login"
+            component={LoginScreen}
+            options={{ headerShown: false }}
+          />
+        ) : (
+          <Stack.Screen
+            name="Splash"
+            component={Splash}
+            options={{ headerShown: false }}
+          />
+        )}
+        <Stack.Screen
+          name="ForgotPassword"
+          component={ForgotPass}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen component={BottomTab} name="BottomTab" />
+        <Stack.Screen component={Centres} name="Centres" />
+        <Stack.Screen component={More} name="More" />
       </Stack.Navigator>
     </NavigationContainer>
   );
