495e72cbbdce1675b2d10518bbce3d5922001341
HoKhua22 <khua.ho22@student.passerellesnumeriques.org>
2022-01-08 09:58:24 +0700

    Implementing centre

--- Screens/Centres.js
+++ Screens/Centres.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, ScrollView } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 import Header from '../components/Header';
 export default function Centres() {
   return (
@@ -11,6 +12,32 @@ export default function Centres() {
           title="Goodstart Early Learning ABC"
           iconLeft="add-box"
         />
+        <View>
+          <ScrollView horizontal={true} style={{ padding: 20 }}>
+            <View style={{ paddingVertical: 10 }}>
+              <View style={{ flexDirection: 'row' }}>
+                <MaterialCommunityIcons
+                  name="storefront-outline"
+                  size={20}
+                  color="#DB147F"
+                />
+                <Text style={{ marginLeft: 10 }}>Total Centres</Text>
+              </View>
+              <Text>Khua</Text>
+            </View>
+            <View style={{ paddingVertical: 10 }}>
+              <View style={{ flexDirection: 'row' }}>
+                <MaterialCommunityIcons
+                  name="storefront-outline"
+                  size={20}
+                  color="#DB147F"
+                />
+                <Text style={{ marginLeft: 10 }}>Total Centres</Text>
+              </View>
+              <Text>Khua</Text>
+            </View>
+          </ScrollView>
+        </View>
       </View>
     </SafeAreaView>
   );
