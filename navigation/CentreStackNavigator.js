783c60928ddfbb585eabf260db0bdb9160a255e2
HoKhua22 <khua.ho22@student.passerellesnumeriques.org>
2022-01-10 17:24:48 +0700

    Top Tab Style

--- navigation/CentreStackNavigator.js
+++ navigation/CentreStackNavigator.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Centres from '../Screens/Centres';
 import TopTabNavigator from './TopTabNavigator';
-// import CentreDetail from '../Screens/CentreDetail';
 
 const CentreStack = createNativeStackNavigator();
 
@@ -10,14 +9,18 @@ const CentreStackNavigator = () => {
   return (
     <CentreStack.Navigator
       initialRouteName="Centre"
-      screenOptions={{ headerTitleAlign: 'center' }}
+      screenOptions={{
+        headerTitleAlign: 'center',
+        headerTitleStyle: {
+          fontSize: 18,
+        },
+      }}
     >
       <CentreStack.Screen
         name="Centre"
         component={Centres}
         options={{ headerShown: false }}
       />
-      {/* <CentreStack.Screen name="Centre Details" component={CentreDetail} /> */}
       <CentreStack.Screen name="Centre Details" component={TopTabNavigator} />
     </CentreStack.Navigator>
   );
--- navigation/TopTabNavigator.js
+++ navigation/TopTabNavigator.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View, ScrollView } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 import Summary from '../Screens/Tab/Summary';
 import Centre_Info from '../Screens/Tab/Centre_Info';
@@ -8,7 +8,6 @@ import Service from '../Screens/Tab/Service';
 import Feature from '../Screens/Tab/Feature';
 import Marketing from '../Screens/Tab/Marketing';
 import Ratings_Reviews from '../Screens/Tab/Ratings_Reviews';
-import MyTabBar from '../components/MyTabBar';
 
 const TabCentre = createMaterialTopTabNavigator();
 
@@ -16,21 +15,208 @@ const TopTabNavigator = () => {
   return (
     <TabCentre.Navigator
       initialRouteName="Summary"
-      // tabBar={(props) => <MyTabBar {...props} />}
       screenOptions={{
-        tabBarActiveTintColor: '#e91e63',
-        tabBarLabelStyle: { fontSize: 12 },
         tabBarScrollEnabled: true,
-        // tabBarStyle: { backgroundColor: 'gray' },
+        tabBarShowLabel: false,
+        tabBarStyle: { backgroundColor: '#E5E5E5' },
       }}
     >
-      <TabCentre.Screen name="Summary" component={Summary} />
-      <TabCentre.Screen name="Centre Information" component={Centre_Info} />
-      <TabCentre.Screen name="Hours" component={Hours} />
-      <TabCentre.Screen name="Services" component={Service} />
-      <TabCentre.Screen name="Features" component={Feature} />
-      <TabCentre.Screen name="Marketing" component={Marketing} />
-      <TabCentre.Screen name="Rating Review" component={Ratings_Reviews} />
+      <TabCentre.Screen
+        name="Summary"
+        component={Summary}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <TouchableOpacity
+              style={{
+                width: 96,
+                marginLeft: -40,
+                paddingHorizontal: 20,
+                paddingVertical: 5,
+                backgroundColor: focused ? '#fff' : '',
+                borderRadius: 6,
+              }}
+            >
+              <Text
+                style={{
+                  color: focused ? '#e91e63' : '#857E7F',
+                  fontSize: 11,
+                  fontWeight: 'bold',
+                }}
+              >
+                SUMMARY
+              </Text>
+            </TouchableOpacity>
+          ),
+        }}
+      />
+      <TabCentre.Screen
+        name="Centre Information"
+        component={Centre_Info}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <TouchableOpacity
+              style={{
+                width: 160,
+                marginLeft: -68,
+                paddingHorizontal: 20,
+                paddingVertical: 5,
+                backgroundColor: focused ? '#fff' : '',
+                borderRadius: 6,
+              }}
+            >
+              <Text
+                style={{
+                  color: focused ? '#e91e63' : '#857E7F',
+                  fontSize: 11,
+                  fontWeight: 'bold',
+                }}
+              >
+                CENTRE INFORMATION
+              </Text>
+            </TouchableOpacity>
+          ),
+        }}
+      />
+      <TabCentre.Screen
+        name="Hours"
+        component={Hours}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <TouchableOpacity
+              style={{
+                width: 78,
+                marginLeft: -27,
+                paddingHorizontal: 20,
+                paddingVertical: 5,
+                backgroundColor: focused ? '#fff' : '',
+                borderRadius: 6,
+              }}
+            >
+              <Text
+                style={{
+                  color: focused ? '#e91e63' : '#857E7F',
+                  fontSize: 11,
+                  fontWeight: 'bold',
+                }}
+              >
+                HOURS
+              </Text>
+            </TouchableOpacity>
+          ),
+        }}
+      />
+      <TabCentre.Screen
+        name="Services"
+        component={Service}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <TouchableOpacity
+              style={{
+                width: 92,
+                marginLeft: -35,
+                paddingHorizontal: 20,
+                paddingVertical: 5,
+                backgroundColor: focused ? '#fff' : '',
+                borderRadius: 6,
+              }}
+            >
+              <Text
+                style={{
+                  color: focused ? '#e91e63' : '#857E7F',
+                  fontSize: 11,
+                  fontWeight: 'bold',
+                }}
+              >
+                SERVICES
+              </Text>
+            </TouchableOpacity>
+          ),
+        }}
+      />
+      <TabCentre.Screen
+        name="Features"
+        component={Feature}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <TouchableOpacity
+              style={{
+                width: 94,
+                marginLeft: -37,
+                paddingHorizontal: 20,
+                paddingVertical: 5,
+                backgroundColor: focused ? '#fff' : '',
+                borderRadius: 6,
+              }}
+            >
+              <Text
+                style={{
+                  color: focused ? '#e91e63' : '#857E7F',
+                  fontSize: 11,
+                  fontWeight: 'bold',
+                }}
+              >
+                FEATURES
+              </Text>
+            </TouchableOpacity>
+          ),
+        }}
+      />
+      <TabCentre.Screen
+        name="Marketing"
+        component={Marketing}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <TouchableOpacity
+              style={{
+                width: 105,
+                marginLeft: -42,
+                paddingHorizontal: 20,
+                paddingVertical: 5,
+                backgroundColor: focused ? '#fff' : '',
+                borderRadius: 6,
+              }}
+            >
+              <Text
+                style={{
+                  color: focused ? '#e91e63' : '#857E7F',
+                  fontSize: 11,
+                  fontWeight: 'bold',
+                }}
+              >
+                MARKETING
+              </Text>
+            </TouchableOpacity>
+          ),
+        }}
+      />
+      <TabCentre.Screen
+        name="Rating Review"
+        component={Ratings_Reviews}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <TouchableOpacity
+              style={{
+                width: 125,
+                marginLeft: -47,
+                paddingHorizontal: 20,
+                paddingVertical: 5,
+                backgroundColor: focused ? '#fff' : '',
+                borderRadius: 6,
+              }}
+            >
+              <Text
+                style={{
+                  color: focused ? '#e91e63' : '#857E7F',
+                  fontSize: 11,
+                  fontWeight: 'bold',
+                }}
+              >
+                RATING REVIEW
+              </Text>
+            </TouchableOpacity>
+          ),
+        }}
+      />
     </TabCentre.Navigator>
   );
 };
