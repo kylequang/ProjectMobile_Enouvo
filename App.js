b36567d87c50f6a3e1890eb46642ce3860f9b924
HoKhua22 <khua.ho22@student.passerellesnumeriques.org>
2022-01-17 16:58:53 +0700

    Updating code

--- App.js
+++ App.js
@@ -1,8 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import useFonts from './src/hooks/useFonts';
-import AppLoading from 'expo-app-loading';
 import LoginScreen from './src/screens/auth/LoginScreen';
 import SplashScreen from './src/screens/auth/SplashScreen';
 import ForgotPassScreen from './src/screens/auth/ForgotPassScreen';
@@ -12,24 +10,11 @@ import MoreStackNavigator from './src/navigations/MoreStackNavigator';
 const Stack = createNativeStackNavigator();
 export default function App() {
   const [timePassed, setTimePassed] = useState(false);
-  const [IsReady, SetIsReady] = useState(false);
 
   useEffect(() => {
     setTimeout(() => setTimePassed({ timePassed: true }), 10000);
   }, []);
 
-  const LoadFontsAndRestoreToken = async () => {
-    await useFonts();
-  };
-
-  if (!IsReady) {
-    return (
-      <AppLoading
-        startAsync={LoadFontsAndRestoreToken}
-        onFinish={() => SetIsReady(true)}
-      />
-    );
-  }
   return (
     <NavigationContainer>
       <Stack.Navigator>
